ab18750ed8d8b5b913ac70a2722cc01e852d76a6
Jaume <ballestertomasj@gmail.com>
2022-01-19 11:42:40 +0100

    useReduce

--- src/App.js
+++ src/App.js
@@ -1,34 +1,6 @@
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
 
-const useContador = (inicial) => {
-  const [contador, setContador] = useState(inicial);
-  const incrementar = () => {
-    setContador(contador + 1);
-  };
-  return [contador, incrementar];
-};
-
-const Interval = ({ contador }) => {
-  useEffect(() => {
-    const i = setInterval(() => console.log(contador), 1000);
-    return () => clearInterval(i);
-  }, [contador]);
-  return <p>Intervalo</p>;
-};
-
-const App = () => {
-  const [contador, incrementar] = useContador(0);
-  useEffect(() => {
-    document.title = contador;
-    //console.log("soy un efecto");
-  }, [contador]);
-  return (
-    <div>
-      Contador:{contador}
-      <button onClick={incrementar}>Incrementar</button>
-      <Interval contador={contador} />
-    </div>
-  );
-};
+// const state = { contador : 0 }
+// action = { type : string }
 
 export default App;
