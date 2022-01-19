fd15c94619f27cf5cb35409fcb6cbd0a0666a165
Jaume <ballestertomasj@gmail.com>
2022-01-19 11:56:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import { useReducer } from "react";
 
 // const state = { contador : 0 }
 // action = { type : string }
+const inicial = { contador: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "incrementar":
+      return { contador: state.contador + 1 };
+    case "decrementar":
+      return { contador: state.contador - 1 };
+    case "set":
+      return { contador: action.payload };
+  }
+};
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, inicial);
+
+  return (
+    <div>
+      Contador: {state.contador}
+      <button onClick={() => dispatch({ type: "incrementar" })}>Mas</button>
+      <button onClick={() => dispatch({ type: "decrementar" })}>Menos</button>
+    </div>
+  );
+};
 
 export default App;
