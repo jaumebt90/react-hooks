777bb65f7ab3d2e4a21b20a07762f940df74309c
Jaume <ballestertomasj@gmail.com>
2022-01-19 09:35:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+const impura = () => new Date().toLocaleDateString();
+
+console.log(impura());
+
+// funcion pura, nunca cambia su valor de retorno
+const MiComponente = ({ miProp }) => {
+  return <div>Nombre:{miProp}</div>;
+};
+const App = () => {
+  return <MiComponente miProp={"chanchito feliz"} />;
+};
+
 export default App;
